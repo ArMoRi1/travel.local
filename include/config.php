6823683caeacd17604d58192c313d68585d68527
Artem <artemmochalov445@icloud.com>
2023-05-28 16:23:23 +0300

    Частина 1(комміт 1)
    Додав розмітку на index.php і скорегував код config.php

--- include/config.php
+++ include/config.php
@@ -1,13 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
-</body>
-</html>
+<?php
+$conn = mysqli_connect('localhost', 'root', '', 'travel.local');
+if (mysqli_connect_errno()){
+    echo 'Помилка підключення до БД ('.mysqli_connect_errno()
+        .'):'.mysqli_connect_error();
+    exit();
+}
